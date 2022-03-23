c5aed08ce755c28f14330b97b1ea1b68e8c0494d
carlocontardi <c.contardi@icloud.com>
2022-03-23 09:47:23 +0100

    .

--- leggi_checkbox.php
+++ leggi_checkbox.php
@@ -5,6 +5,14 @@ if(isset($_POST['linguaggi'])){
     foreach ($linguaggi as $linguaggio){
         echo $linguaggio."<br>";
     }
+    echo count($linguaggi)."<br>";
+    echo sizeof($linguaggi);
+    /*
+    for($i=0;$i<count($linguaggi);$i++){
+        echo $linguaggi[$i]."<br>";
+    }
+    */
+
 }else{
     echo "Non hai selezionato nulla";
 }
--- linguaggi.php
+++ linguaggi.php
@@ -1,12 +1,13 @@
 <?php
 ?>
 <form action="leggi_checkbox.php" method="post">
-    <input type="checkbox" name="linguaggi[]" value="Java" checked>Java<br>
+    <input type="checkbox" name="linguaggi[]" value="Java" >Java<br>
     <input type="checkbox" name="linguaggi[]" value="C++">C++<br>
     <input type="checkbox" name="linguaggi[]" value="PHP">PHP<br>
     <input type="checkbox" name="linguaggi[]" value="C#">C#<br>
     <input type="submit">
 </form>
+
 
 
 
